1712e315c70bb27f702ee50c3c0389aa993cd8d4
SanzhiEr <sanzhi.erdyneev@gmail.com>
2024-02-15 01:51:29 +0800

    Lesson 10 - добавление autowired переменной, конструктору, сеттеру

---
+++ src/main/java/ru/erdyneev/springcourse/ClassicalMusic.java
@@ -0,0 +1,12 @@
+package ru.erdyneev.springcourse;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class ClassicalMusic implements Music {
+
+    @Override
+    public String getSong() {
+        return "Hungarian Rhapsody";
+    }
+}
