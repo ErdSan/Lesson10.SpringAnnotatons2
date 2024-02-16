02ebd3189a28b8f7d6b721b78a515f35727f4794
SanzhiEr <sanzhi.erdyneev@gmail.com>
2024-02-17 02:18:44 +0800

    Lesson 12

--- src/main/java/ru/erdyneev/springcourse/ClassicalMusic.java
+++ src/main/java/ru/erdyneev/springcourse/ClassicalMusic.java
@@ -1,9 +1,23 @@
 package ru.erdyneev.springcourse;
 
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 @Component
+@Scope("prototype")
 public class ClassicalMusic implements Music {
+    @PostConstruct
+    public void doMyInit() {
+        System.out.println("do init");
+    }
+
+    @PreDestroy
+    public void doMyDestroy() {
+        System.out.println("do destroy");
+    }
 
     @Override
     public String getSong() {
--- src/main/java/ru/erdyneev/springcourse/TestSpring.java
+++ src/main/java/ru/erdyneev/springcourse/TestSpring.java
@@ -5,11 +5,11 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class TestSpring {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
-        //MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
-        //musicPlayer.playMusic();
+        MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+        System.out.println(musicPlayer.getName());
+        System.out.println(musicPlayer.getVolume());
 
-        Computer computer = context.getBean("computer", Computer.class);
-        System.out.println(computer);
+        ClassicalMusic classicalMusic1 = context.getBean("classicalMusic", ClassicalMusic.class);
 
         context.close();
     }
